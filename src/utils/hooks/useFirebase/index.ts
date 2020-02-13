6701ddbf9bf439552c8abd08408a573835478566
Akuoko Daniel Jnr <danielakuokojnr@gmail.com>
2020-02-13 03:37:11 +0000

    feat: add firebase hook

---
+++ src/utils/hooks/useFirebase/index.ts
@@ -0,0 +1,13 @@
+import { useEffect, useState} from "react";
+
+import getFirebase from "./firebase";
+
+export default function() {
+    const [instance, setInstance] = useState(null);
+
+    useEffect(() => {
+        setInstance(getFirebase());
+    }, [])
+
+    return instance;
+}
