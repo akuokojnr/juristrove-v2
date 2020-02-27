8cf7bf46c94d5487ce152e96797d7dc5ea62dcb3
Akuoko Daniel Jnr <danielakuokojnr@gmail.com>
2020-02-27 11:02:25 +0000

    fix: dashboard footer

--- src/components/common/wrapper.tsx
+++ src/components/common/wrapper.tsx
@@ -4,6 +4,7 @@ import media from "utils/media";
 
 const Wrapper = styled.div`
   max-width: 1380px;
+  min-height: calc(100vh - 9rem);
   margin-right: auto;
   margin-left: auto;
   padding: 2rem 1.5rem;
